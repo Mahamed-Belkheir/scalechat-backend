328a6931c85a1b2652d85ac1a320969526207566
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-04-25 23:10:42 +0200

    feat(socket_service): added websocket connection and request handlers

---
+++ socket_service/api/connection.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"sync"
+
+	service "github.com/Mahamed-Belkheir/scalechat-backend/socket_service"
+	"github.com/Mahamed-Belkheir/scalechat-backend/socket_service/broker"
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
+)
+
+type connection struct {
+	userId      string
+	roomName    string
+	con         *websocket.Conn
+	br          *broker.MessageBroker
+	sendQueue   chan service.Message
+	closeSignal chan bool
+	wg          *sync.WaitGroup
+	once        *sync.Once
+}
+
+func newConnection(userId, roomName string, con *websocket.Conn, br *broker.MessageBroker) connection {
+	return connection{
+		userId,
+		roomName,
+		con,
+		br,
+		make(chan service.Message),
+		make(chan bool),
+		&sync.WaitGroup{},
+		&sync.Once{},
+	}
+}
+
+func (c *connection) exit() {
+	c.br.Unregister(c.userId, c.roomName)
+	close(c.closeSignal)
+	close(c.sendQueue)
+}
+
+func (c *connection) Run() {
+	c.br.Register(c.userId, c.roomName, c.sendQueue)
+	c.wg.Add(2)
+	go c.recieve()
+	go c.transmit()
+	c.wg.Wait()
+}
+
+func (c *connection) recieve() {
+	defer c.wg.Done()
+	for {
+		select {
+		case <-c.closeSignal:
+			return
+		default:
+			var msg service.Message
+			err := wsjson.Read(context.Background(), c.con, &msg)
+			if err != nil {
+				log.Printf("error: client %v ran into error %v", c.userId, err)
+				c.once.Do(c.exit)
+				return
+			}
+			msg.UserID = c.userId
+			c.br.SendMessage(msg)
+		}
+	}
+}
+
+func (c *connection) transmit() {
+	defer c.wg.Done()
+	for {
+		select {
+		case <-c.closeSignal:
+			return
+		case msg := <-c.sendQueue:
+			res, err := json.Marshal(msg)
+			if err != nil {
+				log.Printf("error: parsing json %v", err)
+				c.once.Do(c.exit)
+				return
+			}
+			c.con.Write(context.Background(), websocket.MessageText, res)
+		}
+	}
+}
---
+++ socket_service/api/ws.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"net/http"
+
+	service "github.com/Mahamed-Belkheir/scalechat-backend/socket_service"
+	"github.com/Mahamed-Belkheir/scalechat-backend/socket_service/broker"
+	"github.com/Mahamed-Belkheir/scalechat-backend/socket_service/pool"
+	"nhooyr.io/websocket"
+)
+
+type handler struct {
+	br     *broker.MessageBroker
+	pool   *pool.Pool
+	config service.Config
+	jwt    JWT
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	roomName := r.URL.Query().Get("name")
+	if roomName == "" {
+		http.Error(w, "name is a required query parameter", http.StatusBadRequest)
+		return
+	}
+	userId, err := h.jwt.verify(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if h.pool.IsFull() {
+		http.Error(w, "service unavilable", http.StatusServiceUnavailable)
+		return
+	}
+	conn, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	work := newConnection(userId, roomName, conn, h.br)
+	h.pool.AddJob(&work)
+}
