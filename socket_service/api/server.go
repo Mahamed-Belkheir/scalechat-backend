850cd6c0b5f3d4a04cfd89d520214a21d8662802
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-07-10 15:36:51 +0200

    feat(socket_service): added logging to webserver startup

--- socket_service/api/server.go
+++ socket_service/api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	service "github.com/Mahamed-Belkheir/scalechat-backend/socket_service"
@@ -8,8 +9,9 @@ import (
 	"github.com/Mahamed-Belkheir/scalechat-backend/socket_service/pool"
 )
 
-func StartWebServer(config service.Config, br *broker.MessageBroker, pool *pool.Pool) error {
+func StartWebServer(config service.Config, br *broker.MessageBroker, pool *pool.Pool) {
 	jwt := NewJWT(config)
 	http.Handle("/ws", newWSHandler(br, pool, config, jwt))
-	return http.ListenAndServe(config.Port, nil)
+	log.Printf("info: server listening at %v", config.Port)
+	log.Fatal(http.ListenAndServe(config.Port, nil))
 }
