2fa3eb6cb00038d05ff2f139f9eea12b11e44936
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-07-12 19:07:42 +0200

    feat(docker): added base docker file and the 3 services dockerfiles

---
+++ user_service/cmd/migrate/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/Mahamed-Belkheir/scalechat-backend/user_service"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/github"
+)
+
+func main() {
+	config := user_service.GetConfig()
+	p, err := filepath.Abs(".")
+	if err != nil {
+		log.Fatalf("failed to make get path %v", err)
+	}
+	p = filepath.ToSlash(p)
+	p = path.Join(p, "migrations")
+	m, err := migrate.New(
+		fmt.Sprintf("file://%s", p),
+		config.DBConn)
+	if err != nil {
+		log.Fatalf("failed to make migrate instance %v", err)
+	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "up":
+		err = m.Up()
+	case "down":
+		err = m.Down()
+	default:
+		log.Fatalf("incorrect command %v", cmd)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
