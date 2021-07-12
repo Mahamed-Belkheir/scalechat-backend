4b9ee530ea77f8d97a11b8d8c9a628e5aeb3c47c
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-07-12 19:10:50 +0200

    feat(docker): added a shell script to build all 4 docker files

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-#env! bash
+docker build -f docker/Dockerfile.base -t scalechat_base:0.1 .;
+docker build -f docker/Dockerfile.chat -t scalechat_chat:0.1 .;
+docker build -f docker/Dockerfile.socket -t scalechat_socket:0.1 .;
+docker build -f docker/Dockerfile.user -t scalechat_user:0.1 .;
